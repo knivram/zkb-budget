b89fc7257aa7e03c4288444f617e478ca20f9211
knivram <129607867+knivram@users.noreply.github.com>
2025-12-29 22:33:32 +0100

    update table to have integer for price and subscribed_at field

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,7 +1,7 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_normal_lucky_pierre.sql';
+import m0000 from './0000_lean_white_queen.sql';
 
   export default {
     journal,
