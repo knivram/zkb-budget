29757ed800f9fbc0ed91cad1248bdd16791d35a9
knivram <129607867+knivram@users.noreply.github.com>
2025-12-29 22:33:32 +0100

    update table to have integer for price and subscribed_at field

--- db/schema.ts
+++ db/schema.ts
@@ -1,19 +1,19 @@
-import { sqliteTable, text, real, integer } from 'drizzle-orm/sqlite-core';
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const subscriptions = sqliteTable('subscriptions', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  name: text('name').notNull(),
-  price: real('price').notNull(),
-  billingCycle: text('billing_cycle', { 
-    enum: ['weekly', 'monthly', 'yearly'] 
+export const subscriptions = sqliteTable("subscriptions", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  price: integer("price").notNull(), // in cents
+  billingCycle: text("billing_cycle", {
+    enum: ["weekly", "monthly", "yearly"],
   }).notNull(),
-  createdAt: integer('created_at', { mode: 'timestamp' })
+  subscribedAt: integer("subscribed_at", { mode: "timestamp" }).notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" })
     .notNull()
     .$defaultFn(() => new Date()),
-  url: text('url'),
-  icon: text('icon'),
+  url: text("url"),
+  icon: text("icon"),
 });
 
 export type Subscription = typeof subscriptions.$inferSelect;
 export type NewSubscription = typeof subscriptions.$inferInsert;
-
--- drizzle/0000_lean_white_queen.sql
+++ drizzle/0000_lean_white_queen.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `subscriptions` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
-	`price` real NOT NULL,
+	`price` integer NOT NULL,
 	`billing_cycle` text NOT NULL,
+	`subscribed_at` integer NOT NULL,
 	`created_at` integer NOT NULL,
 	`url` text,
 	`icon` text
