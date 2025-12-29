61bb0446b28299abe49f0f543548ee3fd21ed61d
knivram <129607867+knivram@users.noreply.github.com>
2025-12-29 22:41:45 +0100

    add to do for handling db loading/migration errors

--- db/client.ts
+++ db/client.ts
@@ -1,10 +1,10 @@
-import { drizzle } from 'drizzle-orm/expo-sqlite';
-import { openDatabaseSync } from 'expo-sqlite';
-import * as schema from './schema';
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { openDatabaseSync } from "expo-sqlite";
+import * as schema from "./schema";
 
-const expoDb = openDatabaseSync('zkb-budget.db', { 
-  enableChangeListener: true 
+// TODO: #2 handle database loading error
+const expoDb = openDatabaseSync("zkb-budget.db", {
+  enableChangeListener: true,
 });
 
 export const db = drizzle(expoDb, { schema });
-
