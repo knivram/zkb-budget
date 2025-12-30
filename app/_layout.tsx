dccb92bc7516a1cf6239312a7b7ff02367793ea6
knivram <129607867+knivram@users.noreply.github.com>
2025-12-30 14:20:32 +0100

    fix title layout problems

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,25 @@
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
-import { isLiquidGlassAvailable } from "expo-glass-effect";
 import { router, Stack } from "expo-router";
-import { StyleSheet, Text, useColorScheme, View } from "react-native";
+import { Text, View } from "react-native";
 import { db } from "../db/client";
 import migrations from "../drizzle/migrations";
 import "../global.css";
 
 export default function RootLayout() {
-  const rawTheme = useColorScheme();
-  const theme = rawTheme === "dark" ? "dark" : "light";
-  const isGlassAvailable = isLiquidGlassAvailable();
-  const blurEffect =
-    theme === "dark" ? "systemMaterialDark" : "systemMaterialLight";
-
   // TOOD: #2 handle migrations error
   const { success, error } = useMigrations(db, migrations);
 
   if (error) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.errorText}>Migration error: {error.message}</Text>
+      <View className="flex-1 justify-center items-center">
+        <Text className="text-red-500">Migration error: {error.message}</Text>
       </View>
     );
   }
 
   if (!success) {
     return (
-      <View style={styles.container}>
+      <View className="flex-1 justify-center items-center">
         <Text>Loading...</Text>
       </View>
     );
@@ -37,11 +30,7 @@ export default function RootLayout() {
       <Stack.Screen
         name="index"
         options={{
-          headerLargeTitle: true,
-          headerTransparent: true,
-          headerTintColor: theme === "dark" ? "white" : "black",
-          headerLargeStyle: { backgroundColor: "transparent" },
-          headerBlurEffect: isGlassAvailable ? undefined : blurEffect,
+          headerLargeTitleEnabled: true,
           title: "Home",
           unstable_headerRightItems: () => [
             {
@@ -61,23 +50,10 @@ export default function RootLayout() {
         options={{
           presentation: "modal",
           title: "Add Subscription",
-          headerLargeTitle: false,
+          headerLargeTitleEnabled: false,
           headerTransparent: true,
-          headerTintColor: theme === "dark" ? "white" : "black",
-          headerBlurEffect: isGlassAvailable ? undefined : blurEffect,
         }}
       />
     </Stack>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  errorText: {
-    color: "red",
-  },
-});
