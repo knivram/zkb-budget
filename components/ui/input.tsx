af7ad147963ecdc461ac89b5a8e3c759bbb6520a
knivram <129607867+knivram@users.noreply.github.com>
2025-12-30 18:36:58 +0100

    fix input alignment

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,15 +1,23 @@
+import { cn } from "@/lib/utils";
 import { TextInput, TextInputProps } from "react-native";
 
-interface InputProps extends TextInputProps {
-  className?: string;
-}
-
-export function Input({ className = "", ...props }: InputProps) {
+export function Input({ className, ...props }: TextInputProps) {
   return (
     <TextInput
-      className={`text-base text-zinc-900 dark:text-white ${className}`}
-      textAlignVertical="center"
+      className={cn(
+        "text-base text-zinc-900 dark:text-white rounded-xl bg-zinc-100 px-4 dark:bg-zinc-800",
+        className
+      )}
+      style={[
+        {
+          height: 44,
+          lineHeight: 20,
+          paddingVertical: 0,
+          paddingBottom: 4,
+        },
+      ]}
       placeholderTextColor="#a1a1aa"
+      multiline={false}
       {...props}
     />
   );
